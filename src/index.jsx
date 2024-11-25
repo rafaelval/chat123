9d6bbe45ae147ef4fb7d6296169b57c5909e01eb
rafael valbuena <artillero.satanas@gmail.com>
2024-11-25 11:56:15 -0500

    configurado auth0

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Auth0Provider
+    domain="dev-lnoda07jj1wqfmje.us.auth0.com"
+    clientId="4wifbuxHy3orTGX8qAA93KHuDJ3E52z8"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById('root')
+);
+
